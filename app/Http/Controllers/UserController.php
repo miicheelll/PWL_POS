316167edeae31467c8ac6e130ea5541685dc1e4e
Michelle Dorani Shiba <doranimichelle@gmail.com>
2025-03-04 20:30:40 +0700

    JB 4 Praktikum 2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,8 @@ class UserController extends Controller
 {
     public function index() {
         
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        $jumlahPengguna = UserModel::count();
+
+        return view('user', ['jumlahPengguna' => $jumlahPengguna]);
     }
 }
