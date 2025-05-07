b6251b2bbdede7efce2215eb41323669ed034431
Michelle Dorani Shiba <doranimichelle@gmail.com>
2025-05-07 08:41:35 +0700

    JB 11 Praktikum 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,15 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Tymon\JWTAuth\Contracts\JWTSubject;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
-
     public function getJWTIdentifier(){
         return $this->getKey();
     }
@@ -18,41 +16,27 @@ class UserModel extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims(){
         return [];
     }
-    
-    public function image(): Attribute
-    {
-        return Attribute::make(
-            get: fn($image) => asset('storage/posts/' . $image),
-        );
-    }
-
-    use HasFactory;
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
-    protected $hidden = ['password'];
+    protected $fillable = [
+        'username',
+        'nama',
+        'password',
+        'level_id',
+        'image' // tambahan
+    ];
 
-    protected $casts = ['password' => 'hashed'];
-
-    public function level(): BelongsTo
+    public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 
-    public function getRoleName(): string
+    protected function image(): Attribute
     {
-        return $this->level->level_nama;
-    }
-
-    public function hasRole(string $role): bool
-    {
-        return $this->level->level_kode === $role;
-    }
-
-    public function getRole()
-    {
-        return $this->level->level_kode;
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
--- database/migrations/2025_05_07_010154_add_image_to_m_user_table.php
+++ database/migrations/2025_05_07_010154_add_image_to_m_user_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('m_user', function (Blueprint $table) {
-            //
+            $table->string('image');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('m_user', function (Blueprint $table) {
-            //
+            $table->dropColumn('image');
         });
     }
 };
